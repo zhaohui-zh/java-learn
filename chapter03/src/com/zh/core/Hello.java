3e90289686652508563cf42c0ea6b044411f96e3
Zhaohui <zhaohui_zh@foxmail.com>
2020-03-30 11:44:29 +0800

    添加javadoc

---
+++ chapter03/src/com/zh/core/Hello.java
@@ -0,0 +1,13 @@
+package com.zh.core;
+
+/**
+ * Hello类
+ *
+ * @author Zhaohui
+ * @date 2020/3/29
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ chapter03/src/com/zh/core/Wrold.java
@@ -0,0 +1,13 @@
+package com.zh.core;
+
+/**
+ * World类
+ *
+ * @author Zhaohui
+ * @date 2020/3/29
+ */
+public class Wrold {
+    public static void main(String[] args) {
+        System.out.println("World");
+    }
+}
